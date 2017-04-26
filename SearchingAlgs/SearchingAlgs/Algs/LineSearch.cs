c60019c8b11d492cfec1b69a4da3c88a052b8ef0
alloboval <perfeo@tutanota.com>
2017-04-26 17:24:46 +0300

    Project

---
+++ SearchingAlgs/SearchingAlgs/Algs/LineSearch.cs
@@ -0,0 +1,24 @@
+﻿namespace SearchingAlgs.Algs
+{
+    class LineSearch
+    {
+        public static int positionX, positionY;
+
+
+        public static void Search(int[] a, int XKey)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i] == XKey)
+                {
+                    positionX = i;
+                }
+            }
+        }
+
+        public static void Search(int[][] a, int XKey)
+        {
+            //will be
+        }
+    }
+}
---
+++ SearchingAlgs/SearchingAlgs/Program.cs
@@ -0,0 +1,16 @@
+﻿using SearchingAlgs.Algs;
+using System;
+
+namespace SearchingAlgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 123, 543, 654, 34, 645, 75, 234, 98, 23, 1, 5658, 980, 56, 231, 6, 35, 8, 923, 68, 34 };
+            LineSearch.Search(a, 5658);
+            Console.WriteLine(LineSearch.positionX);
+            Console.ReadKey();
+        }
+    }
+}
